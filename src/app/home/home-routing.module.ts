6eef20c0a8ba5eb63ff4a76d699d8cf02076076a
alanserafim <alan.serafim@fatec.sp.gov.br>
2022-09-26 09:18:36 -0300

    login, signup, atualizados

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -4,6 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {path: '', component: HomepageComponent},
+  {
+    path: 'login',
+    loadChildren:()=> import ('../personal-data/personal-data.module').then((m)=> m.PersonalDataModule )
+  },
 ];
 
 @NgModule({

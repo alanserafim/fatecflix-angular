044c4337d7ea5a904b4a340a2911905262b9c100
alanserafim <alan.serafim@fatec.sp.gov.br>
2022-09-27 11:39:38 -0300

    service login e signup

--- src/app/personal-data/signup/signup.component.ts
+++ src/app/personal-data/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { RegistrationService } from './../../registration/registration.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  email = '';
+  cpf = '';
+  nome = '';
+  dataNascimento = '';
+  ra = 1;
+  curso = '';
+  anoIngresso = '';
+  semestreIngresso = '';
+  periodo = '';
+  senha = '';
+  confimacao = '';
+  perfil="user"
 
-  constructor() { }
+
+  constructor(private registServive: RegistrationService) { }
 
   ngOnInit(): void {
   }
 
-}
+  cadastra(){
+    this.registServive.cadastra(
+      this.email,
+      this.cpf,
+      this.nome,
+      //this.dataNascimento,
+      //this.ra,
+      //this.curso,
+      //this.anoIngresso,
+      //this.semestreIngresso,
+      //this.periodo,
+      this.senha,
+      //this.confimacao,
+      this.perfil
+      ).subscribe(()=>{
+      console.log("cadastrado com sucesso");
+    }, (error)=> {
+      alert("Cadastro não realizado!");
+      console.log(error);
+
+    }
+    )
+  }
+  }
--- src/app/registration/registration.service.ts
+++ src/app/registration/registration.service.ts
@@ -10,17 +10,28 @@ export class RegistrationService {
 
 
   constructor(private httpClient: HttpClient) { }
-  cadastra(email: string, cpf: string, nome: string, dataNascimento: string, ra: number, curso: string, anoIngresso: string, semestreIngresso: string, periodo: string, senha: string, confirmacao: string): Observable<any>{
+  cadastra(
+    email: string,
+    cpf: string,
+    nome: string,
+    //dataNascimento: string,
+    //ra: number,
+    //curso: string,
+    //anoIngresso: string,
+    //semestreIngresso: string,
+    //periodo: string,
+    senha: string,
+    //confirmacao: string,
+    perfil: string
+    ): Observable<any>{
     return this.httpClient.post('https://fatecflix-api.herokuapp.com/api/v1/users/signUp', {
-    /*
+    email: email,
     cpf: cpf,
-      email: email,
-      password: senha,
-      name: nome,
-      lastname: nome,
-      dataNascimento: dataNascimento,
-    */
-
+    lastname: nome,
+    name: nome,
+    password: senha,
+    roles: perfil,
+    username: email,
     });
   }
 }

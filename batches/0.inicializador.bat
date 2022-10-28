1c9d4b4b2208be766a1dde84b417f1ba8676e2b0
alanserafim <alan.serafim@fatec.sp.gov.br>
2022-10-28 14:59:21 -0300

    login mobile

---
+++ batches/0.inicializador.bat
@@ -0,0 +1,23 @@
+echo "Incializador do projeto"
+echo "******************"
+echo "atualizando o npm"
+call npm update
+echo "******************"
+echo "Baixando dependências"
+call npm install
+echo "******************"
+echo "Instalndo angular cli"
+call angular-cli
+echo "******************"
+echo "Instalndo angular cli pages"
+call angular-cli-pages
+echo "******************"
+echo "Git Config: Alan"
+call git config
+echo "******************"
+echo "Faça deploy automático clicando em deploy-cli-pages.bat"
+echo "******END************"
+echo "Inicializando o Projeto"
+call ng-serve
+echo "******************"
+PAUSE
---
+++ batches/git config.bat
@@ -0,0 +1,1 @@
+git config --global user.email "alan.serafim@fatec.sp.gov.br" && git config --global user.name "alanserafim"

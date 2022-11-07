3bd87f59406bf774dc75764cbdaaebca6d3397e2
Gabriella-Moraes <gabriellamoraes58@gmail.com>
2022-11-07 11:25:16 -0300

    atualizar e deletar aulas

---
+++ src/app/aulas/atualizar-aulas/atualizar-aulas.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atualizar-aulas',
+  templateUrl: './atualizar-aulas.component.html',
+  styleUrls: ['./atualizar-aulas.component.css']
+})
+export class AtualizarAulasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    // this.aula = new Aula();
+    
+    // this.cursoId = this.route.snapshot.params['id'];
+
+    // this.consultaCurso.getCursoById(this.cursoId)
+    //   .subscribe(data => {
+    //     console.log(data)
+    //     this.aula = data;
+    //   }, error => console.log(error)
+    // );
+  }
+
+}

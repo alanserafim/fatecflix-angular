378b48312d0c65cb06ca91b89efbc8cd48bdf72c
alanserafim <alan.serafim@fatec.sp.gov.br>
2022-12-06 13:10:30 -0300

    Module Shared

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,58 @@
+import { SucessoModule } from './../componentes/sucesso/sucesso.module';
+import { MessagesModule } from './../componentes/messages/messages.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { FooterModule } from '../componentes/footer/footer.module';
+import { HeaderModule } from '../componentes/header/header.module';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+
+  ],
+  exports: [
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+    MatIconModule,
+    MatGridListModule,
+    MatTableModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonToggleModule,
+    MatTabsModule,
+    MatListModule,
+    MatStepperModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    FooterModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HeaderModule,
+    MessagesModule,
+    SucessoModule,
+  ],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'pt-BR' }],
+})
+export class SharedModule { }

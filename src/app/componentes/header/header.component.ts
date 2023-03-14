73bcc0fa4f9ca20c674d4fb75c22a70286f50320
LeiteSS <estoudente.program@gmail.com>
2023-03-14 09:22:15 -0300

    Desenvolvimento da barra de acessibilidade, falta alterar:    a) Cores globais para atender as normas de acessibilidade    b) Aumentar a fonte do texto que se encontra no header e    que possuem algum font-size definido. Por exemplo:       - A fonte size da palavra "Login" não aumenta nem diminui

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener  } from '@angular/core';
 import { UsuarioLogadoService } from 'src/app/services/authentication/usuario-logado/usuario-logado.service';
 import { Observable } from 'rxjs';
 import { UsuarioLogado } from './../../types/UsuarioLogado';
 import { Router } from '@angular/router'
 import { TokenService } from 'src/app/services/authentication/token/token.service';
+import { ETheme } from './ETheme';
 
 //var usuarioLogado$ : UsuarioLogado
 @Component({
@@ -14,6 +15,8 @@ import { TokenService } from 'src/app/services/authentication/token/token.servic
 export class HeaderComponent implements OnInit {
   usuarioLogado$ : Observable <UsuarioLogado> = this.usuarioLogadoService.retornaUsuarioLogado();
   isLoggedIn = this.tokeService.possuiToken();
+  public nome: string = ETheme.NOME_COM_CONTRASTE;
+
   constructor(private usuarioLogadoService: UsuarioLogadoService, private route: Router, private tokeService: TokenService) { }
 
   ngOnInit(): void {
@@ -30,4 +33,61 @@ export class HeaderComponent implements OnInit {
       }
     )
   }
+
+  font_size = 16;
+
+  setFontSize(idd: string) {
+
+     //calc font size
+     if(idd === 'a+'){
+      this.font_size += 1;
+    } else if(idd === 'a-') {
+      this.font_size -= 1;
+    } else {
+      this.font_size = 16;
+    }
+
+    //set font size
+    let htmlRoot:HTMLElement = <HTMLElement> document.getElementsByTagName("body")[0];
+
+    console.log(htmlRoot);
+
+    if (htmlRoot != null) {
+      htmlRoot.style.fontSize = `${this.font_size}px`;
+      console.log(htmlRoot.style.fontSize);
+    }
+
+
+
+  }
+
+  public toogle() {
+    const theme = document.body.classList.toggle('contraste');
+
+    if (theme) {
+      return (this.nome = ETheme.NOME_SEM_CONTRASTE);
+    }
+
+    return (this.nome = ETheme.NOME_COM_CONTRASTE)
+  }
+
+  // atalhos acessibilidade
+  @HostListener('window:keyup', ['$event'])
+  keyEvent(event: KeyboardEvent) {
+    if(event.key == '+'){
+      // Your row selection code
+      console.log(event.key);
+      this.setFontSize("a+")
+    }
+    if(event.key == '-'){
+      // Your row selection code
+      console.log(event.key);
+      this.setFontSize("a-")
+    }
+    if(event.key == 'c'){
+      // Your row selection code
+      console.log(event.key);
+      this.toogle();
+    }
+}
 }

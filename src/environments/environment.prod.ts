2e6d119b045aaa70a4e3a6c69d7ea488fe8b9543
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-04-19 12:16:21 -0300

    vercel deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: true,
   baseUrl: 'https://alanserafim.github.io/fatecflix-angular/',
-  //@ts-ignore
-  api_url: process.env.api_url ||'https://fatecflix.herokuapp.com' //https://fatecflix.onrender.com
+  api_url: 'https://fatecflix.herokuapp.com' //https://fatecflix.onrender.com
 };
-
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   //@ts-ignore
   api_url: process.env.api_url ||'https://fatecflix.herokuapp.com' //https://fatecflix.onrender.com
 };

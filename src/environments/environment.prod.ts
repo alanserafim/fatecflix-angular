f5c50ea4ad39b1c978ff76499755d4ded0ecf5d0
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-06-06 10:14:53 -0300

    definindo ambiente de producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   baseUrl: 'https://alanserafim.github.io/fatecflix-angular/',
-  api_url: process.env.API_URL || 'https://fatecflix.herokuapp.com',
+  api_url: 'https://fatecflix-server.herokuapp.com',
 
   //'https://fatecflix-server.herokuapp.com'
   //'http://18.223.115.200:8080'

c9b376f7d9b17d3c750b09861b942a7c6465a420
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-06-08 22:27:07 -0300

    dialog funcionando

--- src/app/home/login/dialog/dialog.component.ts
+++ src/app/home/login/dialog/dialog.component.ts
@@ -3,31 +3,28 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
-export interface DialogData {
-  animal: string;
-  name: string;
+export enum Perfil {
+  ALUNO,
+  INSTRUTOR,
 }
 
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
-  styleUrls: ['./dialog.component.css']
+  styleUrls: ['./dialog.component.css'],
 })
 export class DialogComponent implements OnInit {
-
   perfilControl = new FormControl('');
   perfil?: string;
 
   constructor(
     public dialogRef: MatDialogRef<LoginHomeComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    @Inject(MAT_DIALOG_DATA) public data: Perfil
   ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,4 +1,4 @@
-import { DialogComponent } from './dialog/dialog.component';
+import { DialogComponent, Perfil } from './dialog/dialog.component';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from 'src/app/services/authentication/login/authentication.service';
@@ -8,93 +8,107 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-login-home',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginHomeComponent implements OnInit {
-
   hide = true;
   email = '';
   senha = '';
 
   constructor(
     private authService: AuthenticationService,
-    private router:Router,
+    private router: Router,
     private _snackBar: MatSnackBar,
     public dialog: MatDialog
-  ) { }
+  ) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   login() {
-    this.authService.autentica(this.email, this.senha).subscribe(()=>{
-    this.openSnackBar2("Login realizado com sucesso","");
-    setTimeout(()=>{this.router.navigate(['user/cursos/menu'])}, 1000)
-
-    }, (error)=> {
-      this.openSnackBar("usuário ou senha inválida","Cadastre-se");
-      console.log(this.email, this.senha);
-      console.log(error);
-    }
-    )
+    this.authService.autentica(this.email, this.senha).subscribe(
+      () => {
+        this.openSnackBar2('Login realizado com sucesso', '');
+        setTimeout(() => {
+          this.router.navigate(['user/cursos/menu']);
+        }, 1000);
+      },
+      (error) => {
+        this.openSnackBar('usuário ou senha inválida', 'Cadastre-se');
+        console.log(this.email, this.senha);
+        console.log(error);
+      }
+    );
   }
 
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
-      duration: 5000
+      duration: 5000,
     });
-    this._snackBar._openedSnackBarRef?.onAction().subscribe(()=>{
-      this.router.navigate(['home/cadastro'])
-    })
+    this._snackBar._openedSnackBarRef?.onAction().subscribe(() => {
+      this.router.navigate(['home/cadastro']);
+    });
   }
 
   openSnackBar2(message: string, action: string) {
     this._snackBar.open(message, action, {
-      duration: 2000
+      duration: 2000,
     });
   }
 
   loginAutomaticoInstrutor() {
-    const email = "instrutor";
-    const senha = "12345678"
-    this.authService.autentica(email, senha).subscribe(()=>{
-      this.router.navigate(['user/cursos/menu']);
-      }, (error)=> {
-        alert("Usuário ou senha inválida");
+    const email = 'instrutor';
+    const senha = '12345678';
+    this.authService.autentica(email, senha).subscribe(
+      () => {
+        this.openSnackBar2('Login realizado com sucesso', '');
+        setTimeout(() => {
+          this.router.navigate(['user/cursos/menu']);
+        }, 500);
+      },
+      (error) => {
+        this.openSnackBar('usuário ou senha inválida', 'Cadastre-se');
         console.log(email, senha);
         console.log(error);
       }
-      )
+    );
   }
 
-
   loginAutomaticoAluno() {
-    const email = "aluno";
-    const senha = "12345678"
-    this.authService.autentica(email, senha).subscribe(()=>{
-      this.router.navigate(['user/cursos/menu']);
-      }, (error)=> {
-        alert("Usuário ou senha inválida");
+    const email = 'aluno';
+    const senha = '12345678';
+    this.authService.autentica(email, senha).subscribe(
+      () => {
+        this.openSnackBar2('Login realizado com sucesso', '');
+        setTimeout(() => {
+          this.router.navigate(['user/cursos/menu']);
+        }, 500);
+      },
+      (error) => {
+        this.openSnackBar('usuário ou senha inválida', 'Cadastre-se');
         console.log(email, senha);
         console.log(error);
       }
-      )
-    }
+    );
+  }
 
-    //Dialog
+  //Dialog
 
-    animal: string = "";
-    name: string = "";
+  perfil: string = '';
 
-    openDialog(): void {
-        const dialogRef = this.dialog.open(DialogComponent, {
-        data: {name: this.name, animal: this.animal},
-      });
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      data: Perfil,
+    });
 
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed');
-        this.animal = result;
-      });
-    }
-
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('The dialog was closed');
+      this.perfil = result;
+      console.log(this.perfil);
+      if (this.perfil === 'aluno') {
+        this.loginAutomaticoAluno();
+      } else if (this.perfil === 'instrutor') {
+        this.loginAutomaticoInstrutor();
+      }
+    });
+  }
 }

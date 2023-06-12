a69a85c0504e9a9180d21f70e1a08ca4884444b3
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-06-11 22:46:45 -0300

    formulario recebendo dados

--- src/app/features/exercicios/exibe-exercicio/alternativas/alternativas.component.ts
+++ src/app/features/exercicios/exibe-exercicio/alternativas/alternativas.component.ts
@@ -1,44 +1,119 @@
+import { map } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { Afirmativa, Exercicio, ExercicioTrueFalse, Resposta } from 'src/app/types/Exercicio';
 
 @Component({
   selector: 'app-alternativas',
   templateUrl: './alternativas.component.html',
-  styleUrls: ['./alternativas.component.css']
+  styleUrls: ['./alternativas.component.css'],
 })
 export class AlternativasComponent implements OnInit {
-
-
   submetido = false;
   correta = false;
   verificador = false;
-  enunciado: string = "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti repellendus sunt nesciunt labore nobis, illo in! Quod temporibus aperiam totam quo doloremque praesentium distinctio, omnis placeat deserunt incidunt! Quasi, accusantium!Lorem ipsum dolor sit amet consectetur, adipisicing elit. Alias eos odit reprehenderit ducimus. Voluptatum incidunt voluptate eos earum enim modi voluptates expedita amet porro, sunt consequatur dolor odio possimus dolores?";
-  proposicao: string = "Escolha a alternativa correta"
-  identificador: string = "A"
-  conteudo: string = "Lorem ipsum dolor sit amet consectetur adipisicinelit. Commodi quos, eaque libero laboriosam veniam eius nemo quod imped"
-  resultado: string = ""
-
-  constructor(private _formBuilder: FormBuilder) { }
-
-  ngOnInit(): void {
+  resultado: string = '';
+  get formAfirmativas(): FormArray {
+    return this.exercicioForm.get('afirmativas') as FormArray;
   }
 
-    exercicioForm = this._formBuilder.group({
-      alternativaA: false,
-      alternativaB: false,
+  exercicio : ExercicioTrueFalse = {
+    exercicioId: 1,
+    titulo: "O que são props em React?",
+    enunciado: "Props são muito importantes para o react, com base na aula responda:",
+    proposicao: "Selecione as alternativas corretas",
+    afirmativas: [
+      {
+        identificador: "A",
+        conteudo: "São funções anônimas",
+        corretiva: "Opção incorreta",
+        categoria: false
+      },
+      {
+        identificador: "B",
+        conteudo: "São hooks de estado",
+        corretiva: "Opção incorreta",
+        categoria: false
+      },
+      {
+        identificador: "C",
+        conteudo: "São paramêtros que podem ser passados nas tags",
+        corretiva: "Opção correta",
+        categoria: true
+      },
+      {
+        identificador: "D",
+        conteudo: "O termo props significa propriedades",
+        corretiva: "Opção correta",
+        categoria: true
+      },
+      {
+        identificador: "E",
+        conteudo: "Refere-se ao ciclo de vida do componente",
+        corretiva: "Opção incorreta",
+        categoria: false
+      },
+    ]
+  }
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    // this.getIdentificador();
+
+      //@ts-ignore
+    this.exercicio.afirmativas.forEach(afirmativa => {
+      //@ts-ignore
+      const FormGroupAfirmativa = this.criarFormGroupAfirmativa(afirmativa),
+      // const formAfirmativas = this.exercicioForm.get('afirmativas') as FormArray;
+      this.formAfirmativas.push(FormGroupAfirmativa);
+
     });
+  }
+
+  //criando o formulário
+  exercicioForm = this._formBuilder.group({
+    //@ts-ignore
+    afirmativas: this._formBuilder.array([])
+  });
 
 
+  postarDados(){
+    const dados = this.exercicioForm.value;
+    console.log(dados);
+
+  }
+
+  criarFormGroupAfirmativa(afirmativa: Afirmativa){
+    return this._formBuilder.group({
+      identificador: [afirmativa.identificador],
+      conteudo: [afirmativa.conteudo],
+      corretiva: [afirmativa.corretiva],
+      categoria: [afirmativa.categoria]
+    })
+  }
+
+
+
+  getRespostas() {
+    console.log(this.exercicioForm);
+    this.verificador = false;
+    const resposta = this.exercicioForm.getRawValue();
+    // if (resposta.alternativaA == true && resposta.alternativaB == false) {
+    //   this.verificador = true;
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+  }
 
   verificarRespostas() {
     this.submetido = true;
-    this.verificador = true;
-    if(this.verificador){
-      this.resultado = "Parabéns, resposta correta!"
+    this.postarDados()
+    if (true) {
+      this.resultado = 'Parabéns, resposta correta!';
     } else {
-      this.resultado = "Ops, resposta incorreta. Tente novamente!"
+      this.resultado = 'Ops, resposta incorreta. Tente novamente!';
     }
-
   }
-
 }
--- src/app/types/Exercicio.ts
+++ src/app/types/Exercicio.ts
@@ -5,3 +5,23 @@ export class Exercicio {
   resposta?: string;
   afirmativas?: Array<string>;
 }
+
+export class ExercicioTrueFalse {
+  exercicioId?: number;
+  titulo?: string;
+  enunciado?: string;
+  proposicao?: string;
+  afirmativas?: Array<Afirmativa>;
+}
+
+export class Afirmativa {
+  identificador?: string;
+  conteudo?: string;
+  corretiva?: string;
+  categoria?: boolean;
+}
+
+export interface Resposta {
+  identificador?: string;
+  categoria?: boolean;
+}

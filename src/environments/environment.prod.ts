44b825f5595620b08107811af8328d3a2380a457
alanserafim <alan.serafim@fatec.sp.gov.br>
2023-06-15 23:32:17 -0300

    merging develop

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   baseUrl: 'https://alanserafim.github.io/fatecflix-angular/',
-  api_url: 'https://fatecflix.herokuapp.com',
+  api_url: 'https://fatecflix-server.herokuapp.com',
   //'https://fatecflix-server.herokuapp.com'
   //'http://18.223.115.200:8080'
   //'https://fatecflix.herokuapp.com'
